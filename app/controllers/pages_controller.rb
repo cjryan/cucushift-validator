acca6950e61c5df8dd7de4a1efda3d292dfb689b
Chris Ryan <cryan@redhat.com>
2013-02-19 15:49:15 +0100

    Test database run.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,27 @@
 class PagesController < ApplicationController
   def home
 	@title = "Cucushift Validator"
-
-	@val_code = params[:user_code]	
+        @result = []
+        line_number = 0 
+	@val_code = params[:user_code]
+        
+	#Iterate over each line of code	
+	@val_code.split("\n").each do |line|
+	    #the verification
+            @result.push (false)
+	    Re.all.each do |str|
+              str.sub!(/^\//) #remove /.../
+              str.sub!(/\\$//) #remove /.../
+	      if line =~ /#{str}/
+                 @result[line_number] = true
+                 break
+              end
+	    end
+            line_number += 1
+	end
 
 	#@re = Re.find(10)
+        @result
   end
 
 end
