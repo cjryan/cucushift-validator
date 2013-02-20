0348f0cab0034788a6f1d655b64dfd74f01f8108
Chris Ryan <cryan@redhat.com>
2013-02-20 14:32:24 +0100

    test push

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,7 +16,7 @@ class PagesController < ApplicationController
 	    line = line.strip
 
 	    #remove leading words: When, Then, And, or Given
-	    line.gsub!(/^When/,'Zvezda')
+	    line.sub!(/^When/,'Zvezda')
 
 	    Re.all.each do |str|
               re = str[:re_value]
