817d61cddeceedef5d5be676a1c25bd7301c1792
Chris Ryan <cryan@redhat.com>
2013-02-20 13:28:48 +0100

    Test push.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,26 +2,26 @@ class PagesController < ApplicationController
   def home
 	@title = "Cucushift Validator"
 	@val_code = params[:user_code]
-        @result = []
+        @result = {}
         return 0 unless @val_code
 
         line_number = 0 
         
 	#Iterate over each line of code	
-	@val_code.split("\n").each do |line|
+	@val_code.split("\n").each_with_index do |line, line_number|
 	    #the verification
-            @result.push (false)
+	    next unless line.length > 1
+            @result[line_number] = false
 	    Re.all.each do |str|
               re = str[:re_value]
               re.sub!(/^\//,'') #remove /.../
               re.sub!(/\/$/,'') #remove /.../
 	      if line =~ /#{re}/
                    #@result[line_number] = line + ' true'
-                   @result.push (line + line_number + ' true')
-                 #break
+            	   @result[line_number] = true
+                   break
               end
 	    end
-            line_number += 1
 	end
 	#@re = Re.find(10)
   end
