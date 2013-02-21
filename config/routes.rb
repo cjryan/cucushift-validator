5969691388de5099c9a06631c165504fa91e8da9
Chris Ryan <cryan@redhat.com>
2013-02-21 11:40:10 +0100

    Test push.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RailsApp::Application.routes.draw do
+  root :to => 'pages#home'
   get "pages/home"
   
   #This connects the controller to the view
@@ -53,7 +54,6 @@ RailsApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
