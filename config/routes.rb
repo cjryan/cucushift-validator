98aac53cfc055b85ec196c0c697e2050b72881c6
Michal Zimen <mzimen@redhat.com>
2013-02-26 19:11:20 +0100

    mzimen

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ RailsApp::Application.routes.draw do
   get "pages/push"
   #This connects the controller to the view
   match "/pages/:id" => "pages#home"
+  match "push" => "pages#push", :via => [:post]
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
