b874fb97ab6b3d70ccb6f02c1f42848af9ff90fb
Chris Ryan <cryan@redhat.com>
2013-02-27 14:22:31 +0100

    Fixed the checksum error. There must be two spaces between the hash and the file to be checked.

--- cucushift_db_updater.sh
+++ cucushift_db_updater.sh
@@ -16,23 +16,22 @@ NEWMD5=$(md5sum $FRESH_DUMP | awk '{print $1}')
 
 #Get the mysql value for hash here
 HASH=$(mysql -h $OPENSHIFT_MYSQL_DB_HOST -P $OPENSHIFT_MYSQL_DB_PORT -u $OPENSHIFT_MYSQL_DB_USERNAME -p$OPENSHIFT_MYSQL_DB_PASSWORD cucushiftvalidator --skip-column-names -e "SELECT dbhash FROM step_db_versions;" |tail -1)
-echo "$HASH  cucushift_dump.sql" > list.md5
-cat list.md5
-md5sum -c list.md5
-#RESULT=$(echo "$HASH cucushift_dump.sql" | md5sum -c | awk '{ print $2 }')
+
+#IMPORTANT! There must be TWO (2) spaces between the hash and the file in the md5sum -c check, i.e. $HASH  cucushift_dump.sql in order for the checksum to work.
+RESULT=$(echo "$HASH  cucushift_dump.sql" | md5sum -c | awk '{ print $2 }')
 
 echo "the new md5 is: "$NEWMD5
 echo "the old md5 was: "$HASH
 
 #Compare the result of the hash with the value in the database
-#if [ "$RESULT" = 'OK' ]; then
-#	exit 0
-#	#echo "It's totally cool."
-#else
-#	mysql -h $OPENSHIFT_MYSQL_DB_HOST -P $OPENSHIFT_MYSQL_DB_PORT -u $OPENSHIFT_MYSQL_DB_USERNAME -p$OPENSHIFT_MYSQL_DB_PASSWORD cucushiftvalidator < cucushift_dump.sql
-#	mysql -h $OPENSHIFT_MYSQL_DB_HOST -P $OPENSHIFT_MYSQL_DB_PORT -u $OPENSHIFT_MYSQL_DB_USERNAME -p$OPENSHIFT_MYSQL_DB_PASSWORD cucushiftvalidator -e "UPDATE step_db_versions set version=NOW();"
-#	mysql -h $OPENSHIFT_MYSQL_DB_HOST -P $OPENSHIFT_MYSQL_DB_PORT -u $OPENSHIFT_MYSQL_DB_USERNAME -p$OPENSHIFT_MYSQL_DB_PASSWORD cucushiftvalidator -e "UPDATE step_db_versions set dbhash=\"$NEWMD5\";"
-#fi
+if [ "$RESULT" = 'OK' ]; then
+	exit 0
+	#echo "It's totally cool."
+else
+	mysql -h $OPENSHIFT_MYSQL_DB_HOST -P $OPENSHIFT_MYSQL_DB_PORT -u $OPENSHIFT_MYSQL_DB_USERNAME -p$OPENSHIFT_MYSQL_DB_PASSWORD cucushiftvalidator < cucushift_dump.sql
+	mysql -h $OPENSHIFT_MYSQL_DB_HOST -P $OPENSHIFT_MYSQL_DB_PORT -u $OPENSHIFT_MYSQL_DB_USERNAME -p$OPENSHIFT_MYSQL_DB_PASSWORD cucushiftvalidator -e "UPDATE step_db_versions set version=NOW();"
+	mysql -h $OPENSHIFT_MYSQL_DB_HOST -P $OPENSHIFT_MYSQL_DB_PORT -u $OPENSHIFT_MYSQL_DB_USERNAME -p$OPENSHIFT_MYSQL_DB_PASSWORD cucushiftvalidator -e "UPDATE step_db_versions set dbhash=\"$NEWMD5\";"
+fi
   
 
 exit 0
